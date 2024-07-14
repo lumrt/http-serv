16d65cabed0fd23028c5f87e3a249d4cf55a78d0
lumaret <lumaret@student.42.fr>
2024-07-14 20:00:45 +0200

    GetLocalIP

--- main.go
+++ main.go
@@ -1,32 +1,66 @@
 package main
 
 import (
+	"context"
 	"errors"
 	"fmt"
 	"io"
 	"net/http"
-	"os"
+	// "net"
 )
 
+var ctx = context.Background()
+
+const keyServerAddr = "serverAddr"
+
+
+func GetLocalIP() string {
+	addrs, err := net.InterfaceAddrs()
+	if err != nil {
+		return ""
+	}
+	for _, address := range addrs {
+		// Vérifiez le type d'adresse et si elle n'est pas bouclante
+		if ipnet, ok := address.(*net.IPNet); ok && !ipnet.IP.IsLoopback() {
+			if ipnet.IP.To4() != nil { // Assurez-vous que l'adresse est IPv4
+				return ipnet.IP.String()
+			}
+		}
+	}
+	return ""
+}
+
 func getRoot(w http.ResponseWriter, r *http.Request) {
-	fmt.Printf("got / request\n")
+	ctx := r.Context()
+	
+	fmt.Printf("%s : got / request\n", ctx.Value(keyServerAddr))
 	io.WriteString(w, "This is my Webserv !\n")
 }
 
 func getTest(w http.ResponseWriter, r *http.Request) {
-	fmt.Printf("got /test resquest\n")
+	ctx := r.Context()
+
+	fmt.Printf("%s : got /test resquest\n", ctx.Value(keyServerAddr))
 	io.WriteString(w, "Hi , HTTP !\n")
 }
 
-func main() {
-	http.HandleFunc("/" , getRoot)
-	http.HandleFunc("/test", getTest)
+func getHTML(w http.ResponseWriter, r *http.Request) {
+	ctx := r.Context()
 
-	err := http.ListenAndServe(":3333", nil)
+	fmt.Printf("%s : go /HTML request !",  ctx.Value(keyServerAddr))
+	io.WriteString(w, "Welcome om the main HTML index !\n")
+}
+
+func main() {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", getRoot)
+	mux.HandleFunc("/test", getTest)
+	mux.HandleFunc("/HTML", getHTML)
+	err := http.ListenAndServe(":3333", mux)
+	// si l'erreur correspond au code erreur d'un serveur http ferme
 	if errors.Is(err, http.ErrServerClosed) {
 		fmt.Printf("server closed\n")
 	} else if err != nil {
 		fmt.Printf("error starting server: %s\n", err)
-		os.Exit(1)
 	}
 }	
